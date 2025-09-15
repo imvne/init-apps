32f0ffa7cf64d2f4264e9a70106f51fd1e48dd9e
imvne <imanebeaufils19@gmail.com>
2025-09-15 22:06:50 +0200

    routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App'
+import MentionsLegales from './MentionsLegales'
+import PolitiqueConf from './PolitiqueConf'
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<App />
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<App />} />
+				<Route path='/mentions-legales' element={<MentionsLegales />} />
+				<Route path='/politique-confidentialite' element={<PolitiqueConf />} />
+			</Routes>
+		</BrowserRouter>
 	</StrictMode>
 )
