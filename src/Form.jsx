5d3889019372de200af129fdbf64ef9fa083b17f
imvne <imanebeaufils19@gmail.com>
2025-09-15 18:08:31 +0200

    form netlify

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,34 +1,70 @@
 import styled from 'styled-components'
 
 export default function Form() {
+	const onSubmit = event => {
+		event.preventDefault()
+		const form = event.target
+
+		fetch('/', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+			body: new URLSearchParams(form).toString(),
+		})
+			.then(() => {
+				document.getElementById('form-success').style.display = 'block'
+				form.reset()
+			})
+			.catch(error => alert('Une erreur est survenue : ' + error))
+	}
+
 	return (
-		<FormContainer id='contact'>
-			<FormTitle>Discutons de votre idée</FormTitle>
-			<FormCategory>Qui êtes-vous ?</FormCategory>
-			<FormRow>
-				<Input placeholder='Prénom *' required />
-				<Input placeholder='Nom *' required />
-			</FormRow>
-			<FormRow>
-				<Input placeholder='Email *' type='email' required />
-				<Input placeholder='Numéro de téléphone' />
-			</FormRow>
-			<FormCategory>Quelle est votre idée ?</FormCategory>
-			<FormRow>
-				<Input placeholder='Décrivez votre projet en une phrase *' required />
-			</FormRow>
-			{/* 
+		<>
+			<FormContainer
+				id='contact'
+				name='contact'
+				method='POST'
+				data-netlify='true'
+				data-netlify-honeypot='bot-field'
+				onSubmit={event => onSubmit(event)}
+				netlify>
+				<input type='hidden' name='form-name' value='contact' />
+
+				<p hidden>
+					<label>
+						Ne pas remplir : <input name='bot-field' />
+					</label>
+				</p>
+
+				<FormTitle>Discutons de votre idée</FormTitle>
+				<FormCategory>Qui êtes-vous ?</FormCategory>
+				<FormRow>
+					<Input name='prenom' placeholder='Prénom *' required />
+					<Input name='nom' placeholder='Nom *' required />
+				</FormRow>
+				<FormRow>
+					<Input name='email' placeholder='Email *' type='email' required />
+					<Input name='telephone' placeholder='Numéro de téléphone' />
+				</FormRow>
+				<FormCategory>Quelle est votre idée ?</FormCategory>
+				<FormRow>
+					<Input name='project' placeholder='Décrivez votre projet en une phrase *' required />
+				</FormRow>
+				{/* 
 			<FormRow>
 				<Input placeholder='Date de lancement' />
 				<Input placeholder='Entreprise' />
 			</FormRow> */}
 
-			<SendFormButton>Envoyer mon idée et programmer un appel</SendFormButton>
-		</FormContainer>
+				<SendFormButton type='submit'>Envoyer mon idée</SendFormButton>
+				<FromSentConfirmation id='form-success' style={{ display: 'none' }}>
+					Merci ! Le formulaire a bien été envoyé.
+				</FromSentConfirmation>
+			</FormContainer>
+		</>
 	)
 }
 
-const FormContainer = styled.div`
+const FormContainer = styled.form`
 	width: 100%;
 	background-color: #e1e2e4;
 	padding: 25px;
@@ -127,5 +163,14 @@ const SendFormButton = styled.button`
 		font-size: 20px;
 		padding: 20px 40px;
 		margin: 60px 0;
+	}
+`
+
+const FromSentConfirmation = styled.div`
+	font-family: 'League Spartan';
+	color: #112250ed;
+
+	@media (min-width: 768px) {
+		font-size: 21px;
 	}
 `
